9d8d62e7265ab26e9aec0f4e4f15c32d2e09783c
AbdullahUC <81858363+AbdullahUC@users.noreply.github.com>
2026-01-19 02:16:51 +0600

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,11 +1,11 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
+const swiper = new Swiper('.wrapper', {
+    loop: true,
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    clickable:true,
+    dynamicBullets:true
   },
 
   // Navigation arrows
@@ -14,6 +14,31 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 
-  
-});
+    autoplay : {
+    delay: 2000,
+    pauseOnMouseEnter: true ,
+    disableOnInteraction: true
+  },
 
+
+
+    breakpoints: {
+    // when window width is >= 500px
+    500: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    }
+  }
+
+ 
+});
